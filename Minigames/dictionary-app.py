1c6136e3857e6eb093e689ee5d900ddc515bcc2a
Muhammad <55012876+Muhammadali-gold@users.noreply.github.com>
2021-03-20 15:21:39 +0500

    Update dictionary-app.py

--- Minigames/dictionary-app.py
+++ Minigames/dictionary-app.py
@@ -1,3 +1,12 @@
 import json
 
-data=json.load()
+data=json.load(open('data.json'))
+
+def main():
+    word=input("Please, input search word=").lower()
+    if word in data:
+        print(data[word])
+    else:
+        print("Sorry information not found")
+if __name__ == '__main__':
+    main()
