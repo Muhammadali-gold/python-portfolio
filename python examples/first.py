c2fcaebb259da7c813c647de811b73b8bafec58e
Muhammad <55012876+Muhammadali-gold@users.noreply.github.com>
2021-03-30 11:11:18 +0500

    'committ'

---
+++ python examples/first.py
@@ -0,0 +1,18 @@
+# 1-program
+print("Hello,World")
+
+# in cmd
+# type:python -c "print('Hello, World')"
+# result:Hello, World
+
+# Integer
+a = 2 
+print(a)
+# Output:2
+
+# Integer 
+b = 95634653645626342624326
+print(b)
+# Output:95634653645626342624326
+
+
